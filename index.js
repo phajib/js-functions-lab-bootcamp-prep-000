6e9e795b1e7f9aa946230ba9e48ba1a08e0d528a
H B <phajib@gmail.com>
2018-10-28 07:01:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // write your code below!
+function happyHolidays(){
+return 'Happy holidays!'
+}
+
+function happyHolidaysTo(string){
+  var name = string
+  return 'Happy holidays, ${name}!'
+  if (name === 'Happy holidays, you!'){
+    return 'Happy holidays, you!'
+  }
+}
+
+function happyHolidayTo(string, string){
+  var holiday = string
+  var name = string
+  return 'Happy ${holiday}, ${name}!'
+}
+
+function holidayCountdown(holiday, days){
+  var holiday = string
+  var days = parseint(days)
+  return 'It\'s ${day} days until ${holiday}!'
+  return "It\'s 20 days until Mother's Day"
+}
