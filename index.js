b9cd54a2f679889db90acadce2f46991677b22b7
H B <phajib@gmail.com>
2018-10-28 07:26:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ function happyHolidayTo(holiday, name){
 }
 
 function holidayCountdown(holiday, days){
-  var holiday = string
-  var days = parseint(days)
   return 'It\'s ${day} days until ${holiday}!'
+  holidayCountdown("Mothers Day", 20)
   return "It\'s 20 days until Mother's Day"
 }
